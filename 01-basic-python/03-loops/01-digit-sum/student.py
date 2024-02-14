647b96c30ef04cf82fdecc3dd393978711ff618b
Riad De Pauw <riad.depauw@student.ucll.be>
2024-02-15 00:26:10 +0100

    exercises done all the way till strings

--- 01-basic-python/03-loops/01-digit-sum/student.py
+++ 01-basic-python/03-loops/01-digit-sum/student.py
@@ -1,1 +1,12 @@
 # Write your code here
+def last_digit(n):
+    return n % 10
+
+def remove_last_digit(n):
+    return n // 10
+
+def digit_sum(n):
+    sum = last_digit(n)
+    for digit in str(remove_last_digit(n)):
+        sum += int(digit)
+    return sum
--- 01-basic-python/04-lists/04-middle/student.py
+++ 01-basic-python/04-lists/04-middle/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+def middle(ns):
+    mid = int(len(ns) / 2)
+    return ns[mid]
