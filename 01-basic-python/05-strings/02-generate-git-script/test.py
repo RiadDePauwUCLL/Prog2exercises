3549d62f031f10792c1fa0f960c3e5362ad31bbc
Riad De Pauw <riad.depauw@student.ucll.be>
2024-02-16 18:59:57 +0100

    small changes

---
+++ 01-basic-python/05-strings/02-generate-git-script/test.py
@@ -0,0 +1,16 @@
+from textwrap import dedent
+
+def monkeisgaming123(xd):
+    string = f'''
+    monke is a nice gamer and he is silver 1. {xd}
+        Anyways, he only plays wow, so he sucks.
+            Imagine sucking at wow, really noob
+                b   r   u   h       m   o   m   e   n   t
+                                X
+                                D {xd}
+    '''
+    return dedent(string).strip()
+
+
+
+print(monkeisgaming123("LMAO"))
