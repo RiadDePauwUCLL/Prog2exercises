d5d6fa99b0c7899bb6e0f880e4a23b4ef5f353d0
Riad De Pauw <riad.depauw@student.ucll.be>
2024-02-18 01:44:01 +0100

    remove duplicates

--- 01-basic-python/07-sets/02-remove-duplicates/student.py
+++ 01-basic-python/07-sets/02-remove-duplicates/student.py
@@ -1,4 +1,10 @@
 # Write your code here
 def remove_duplicates(xs):
-    xs = set(xs)
-    
+    seen = set()
+    ys = []
+
+    for i in xs:
+        if i not in seen:
+            ys.append(i)
+            seen.add(i)
+    return ys
