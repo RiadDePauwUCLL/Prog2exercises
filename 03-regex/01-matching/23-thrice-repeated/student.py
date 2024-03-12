2c2a61c76d41ecea30c35b0eefc9df0cb643c80e
Riad De Pauw <riad.depauw@student.ucll.be>
2024-03-12 19:26:32 +0100

    exercises 23,25-26 done

--- 03-regex/01-matching/23-thrice-repeated/student.py
+++ 03-regex/01-matching/23-thrice-repeated/student.py
@@ -4,7 +4,8 @@ import re
 def thrice_repeated(string):
     return re.fullmatch(r'(.+)\1\1', string)
 
-# r at the start denotes a raw string. In raw strings, backslashes are treated as literal characters and not as escape characters. 
+# r at the start denotes a raw string. In raw strings, 
+# backslashes are treated as literal characters and not as escape characters. 
 # This is often used with regular expressions because they frequently contain backslashes.
 
 # '(.+)\1\1' is the regular expression pattern.
@@ -17,5 +18,6 @@ def thrice_repeated(string):
 
 # \1 is a backreference that refers to the contents of the first group.
 
-# So, the regular expression r'(.+)\1\1' matches any string that contains a sequence of one or more characters repeated three times in a row. 
+# So, the regular expression r'(.+)\1\1' matches any string that contains a 
+# sequence of one or more characters repeated three times in a row. 
 # For example, it would match 'abcabcabc' or '111'.
