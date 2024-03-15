baa177f7c5e4f37ab62745b3766ca7fb96f97b87
Riad De Pauw <riad.depauw@student.ucll.be>
2024-03-15 17:44:41 +0100

    time & email address done, password in progress

--- 03-regex/01-matching/28-is-valid-password/student.py
+++ 03-regex/01-matching/28-is-valid-password/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+import re
+
+def is_valid_password(string):
+
+    if re.search(r"(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{12,}", string):
+        if re.search(r"(?!.*?(.)\1\1\1)", string):
+            return True
+    else:
+        return False
